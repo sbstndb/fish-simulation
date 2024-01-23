f15faa96428335a8b33390210e74e3de0ecd0741
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-01-23 11:51:51 +0100

    Add src
    -simple window with ZINZIN subwindow

---
+++ src/test_imgui.cpp
@@ -0,0 +1,84 @@
+#include <SDL.h>
+#include <stdio.h>
+#include "../external/imgui/imgui.h"
+#include "../external/imgui/backends/imgui_impl_sdl2.h"
+#include "../external/imgui/backends/imgui_impl_sdlrenderer2.h"
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Create SDL window and renderer
+    SDL_Window* window = SDL_CreateWindow("SDL2 + ImGui", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Initialize ImGui with SDL and OpenGL2
+    ImGui::CreateContext();
+
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls	
+
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+	io.Fonts->AddFontDefault();
+
+    // Main loop
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+
+            if (event.type == SDL_QUIT)
+                quit = true;
+        }
+
+        // Start a new ImGui frame
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame(window);
+        ImGui::NewFrame();
+
+        // Your ImGui code here
+        ImGui::Begin("Salut les ZINZINS");
+        ImGui::Text("fenetre d'un ZINZIN");
+        ImGui::End();
+
+        // Render ImGui
+        ImGui::Render();
+
+        // Render SDL
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        SDL_RenderPresent(renderer);
+
+        // Wait a short time to avoid excessive CPU usage
+        SDL_Delay(16);
+    }
+
+    // Cleanup
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
